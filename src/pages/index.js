ed0bca9687b7fb490b6eeb189c1da91d0adec48b
Tom Chamberlain <tom_chamberlain_uk@hotmail.co.uk>
2021-06-23 16:09:22 +0100

    Added placeholder content for services and index

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,17 @@
 import * as React from 'react';
+
 import Layout from 'components/layout/Layout';
+import { Container } from 'components/container/Container';
+import { Title, Subtitle, Seperator } from 'components/typography/Typography';
 
 function IndexPage() {
   return (
     <Layout pageTitle="Home">
-      <h1>Home</h1>
+      <Container>
+        <Title>Toby Lyons</Title>
+        <Subtitle>The venerable ser Toby of house Lyons</Subtitle>
+        <Seperator isBold={true} />
+      </Container>
     </Layout>
   );
 }
--- src/pages/services.js
+++ src/pages/services.js
@@ -1,10 +1,52 @@
 import * as React from 'react';
+import { StaticImage } from 'gatsby-plugin-image';
+
 import Layout from 'components/layout/Layout';
+import ParallaxContainer from 'components/parallaxContainer/ParallaxContainer';
+import { Container } from 'components/container/Container';
+import { Title, Subtitle, Heading, Subheading, Paragraph, Note, Seperator, List } from 'components/typography/Typography';
 
 function ServicesPage() {
   return (
     <Layout pageTitle="Services">
-      <h1>Services</h1>
+      <ParallaxContainer images={
+        [
+          <StaticImage
+            src="https://picsum.photos/1920/1080"
+            alt="PLACEHOLDER IMAGE"
+            placeholder="blurred"
+          />,
+          <StaticImage
+            src="https://picsum.photos/seed/x/1920/1080"
+            alt="PLACEHOLDER IMAGE"
+            placeholder="blurred"
+          />
+        ]
+      }>
+        <Container>
+          <Title>Services</Title>
+          <Subtitle>Whatever that entails</Subtitle>
+          <Seperator isBold={true} />
+          <Heading>Heading</Heading>
+          <Paragraph>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vel libero nisi. Sed euismod mattis gravida. In pulvinar, metus id tincidunt aliquam, mauris sapien consequat turpis, id malesuada sem lorem non ligula. Vestibulum dignissim metus nec justo dignissim, egestas suscipit orci tempus. Nunc a dolor urna. Fusce vel metus eget sapien feugiat consectetur sed molestie diam. Maecenas nisl metus, tempus commodo ullamcorper eget, porttitor sed metus. Nulla a arcu eu augue porta tincidunt nec ut felis.</Paragraph>
+          <Paragraph>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vel libero nisi. Sed euismod mattis gravida. In pulvinar, metus id tincidunt aliquam, mauris sapien consequat turpis, id malesuada sem lorem non ligula. Vestibulum dignissim metus nec justo dignissim, egestas suscipit orci tempus. Nunc a dolor urna. Fusce vel metus eget sapien feugiat consectetur sed molestie diam. Maecenas nisl metus, tempus commodo ullamcorper eget, porttitor sed metus. Nulla a arcu eu augue porta tincidunt nec ut felis.</Paragraph>
+          <Subheading>Subheading</Subheading>
+          <List>
+            <List.Item>This is a bullet-point</List.Item>
+            <List.Item>This is a bullet-point</List.Item>
+            <List.Item>This is a bullet-point</List.Item>
+            <List.Item>This is a bullet-point</List.Item>
+          </List>
+          <Seperator />
+          <ParallaxContainer.SectionLabel />
+          <Heading>Heading</Heading>
+          <Paragraph>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vel libero nisi. Sed euismod mattis gravida. In pulvinar, metus id tincidunt aliquam, mauris sapien consequat turpis, id malesuada sem lorem non ligula. Vestibulum dignissim metus nec justo dignissim, egestas suscipit orci tempus. Nunc a dolor urna. Fusce vel metus eget sapien feugiat consectetur sed molestie diam. Maecenas nisl metus, tempus commodo ullamcorper eget, porttitor sed metus. Nulla a arcu eu augue porta tincidunt nec ut felis.</Paragraph>
+          <Paragraph>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vel libero nisi. Sed euismod mattis gravida. In pulvinar, metus id tincidunt aliquam, mauris sapien consequat turpis, id malesuada sem lorem non ligula. Vestibulum dignissim metus nec justo dignissim, egestas suscipit orci tempus. Nunc a dolor urna. Fusce vel metus eget sapien feugiat consectetur sed molestie diam. Maecenas nisl metus, tempus commodo ullamcorper eget, porttitor sed metus. Nulla a arcu eu augue porta tincidunt nec ut felis.</Paragraph>
+          <Paragraph>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vel libero nisi. Sed euismod mattis gravida. In pulvinar, metus id tincidunt aliquam, mauris sapien consequat turpis, id malesuada sem lorem non ligula. Vestibulum dignissim metus nec justo dignissim, egestas suscipit orci tempus. Nunc a dolor urna. Fusce vel metus eget sapien feugiat consectetur sed molestie diam. Maecenas nisl metus, tempus commodo ullamcorper eget, porttitor sed metus. Nulla a arcu eu augue porta tincidunt nec ut felis.</Paragraph>
+          <Seperator />
+          <Note>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vel libero nisi. Sed euismod mattis gravida. In pulvinar, metus id tincidunt aliquam, mauris sapien consequat turpis, id malesuada sem lorem non ligula. Vestibulum dignissim metus nec justo dignissim, egestas suscipit orci tempus. Nunc a dolor urna. Fusce vel metus eget sapien feugiat consectetur sed molestie diam. Maecenas nisl metus, tempus commodo ullamcorper eget, porttitor sed metus. Nulla a arcu eu augue porta tincidunt nec ut felis.</Note>
+        </Container>
+      </ParallaxContainer>
     </Layout>
   );
 }
