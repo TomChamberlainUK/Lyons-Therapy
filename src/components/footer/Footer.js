c18f24b1176f003710fd477b0fa5139038a599fa
Tom Chamberlain <tom_chamberlain_uk@hotmail.co.uk>
2021-07-08 15:56:35 +0100

    updated footer to better represent final product. Added placeholder logo, bacp registry badge, and copyright strip.

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import { StaticImage } from 'gatsby-plugin-image';
 
 import * as styles from './footer.module.scss';
 
 function Footer() {
   return (
     <footer className={styles.footer}>
-      Footer
+      <div className={styles.logo}>LYONS<br />THERAPY</div>
+      <StaticImage
+        src="../../images/bacp_badge_SAMPLE.jpg"
+        alt="SAMPLE BADGE"
+        objectFit="contain"
+        className={styles.bacpBadge}
+      />
+      <div className={styles.legalStrip}>
+        &copy; Lyons Therapy
+      </div>
     </footer>
   )
 }
