1de0a6fd2f4cc5e318c3e99e4f04c7edf5ef5a67
Tom Chamberlain <tom_chamberlain_uk@hotmail.co.uk>
2021-07-14 13:26:39 +0100

    Built Index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,45 @@
 import * as React from 'react';
+import { navigate } from 'gatsby';
 
 import Layout from 'components/layout/Layout';
-import { Container } from 'components/container/Container';
-import { Title, Subtitle, Seperator } from 'components/typography/Typography';
+import { Container, SplitContainer, TextContainer } from 'components/container/Container';
+import { Button, ButtonSet } from 'components/button/Button';
+import SplashGraphic from 'components/splashGraphic/SplashGraphic';
+import { Title, Subtitle, Heading, Paragraph, Seperator } from 'components/typography/Typography';
+
 
 function IndexPage() {
   return (
     <Layout pageTitle="Home">
-      <Container>
-        <Title>Toby Lyons</Title>
-        <Subtitle>The venerable ser Toby of house Lyons</Subtitle>
-        <Seperator isBold={true} />
-      </Container>
+      <SplitContainer
+        subContent={<SplashGraphic />}
+      >
+        <Container>
+          <TextContainer>
+            <Title>Toby Lyons</Title>
+            <Subtitle>BA, PG Cert, PG Dip, MBACP</Subtitle>
+            <Seperator isBold={true} />
+            <Heading>Counselling and Psychotherapy</Heading>
+            <Paragraph>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vel libero nisi. Sed euismod mattis gravida.</Paragraph>
+            <Seperator />
+            <ButtonSet>
+              <Button
+                onClick={() => navigate('/contact')}
+                icon={'markunread'}
+                isPrimary={true}
+              >
+                Get in Touch!
+              </Button>
+              <Button onClick={() => navigate('/about')}>
+                Find Out More
+              </Button>
+              <Button onClick={() => navigate('/services')}>
+                What we Offer
+              </Button>
+            </ButtonSet>
+          </TextContainer>
+        </Container>
+      </SplitContainer>
     </Layout>
   );
 }
