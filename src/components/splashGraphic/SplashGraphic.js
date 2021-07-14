de9bf5f3ac4807df920dddc46e70408099f76275
Tom Chamberlain <tom_chamberlain_uk@hotmail.co.uk>
2021-07-14 13:14:26 +0100

    Added splash graphic component for index page

---
+++ src/components/splashGraphic/SplashGraphic.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { StaticImage } from 'gatsby-plugin-image';
+
+import * as styles from './splashGraphic.module.scss';
+
+export default function SplashGraphic() {
+  return (
+    <div className={styles.container}>
+      <StaticImage
+        src="../images/logo-circular-accent.png"
+        alt="Logo"
+        objectFit="contain"
+        className={styles.logo}
+      />
+      <div className={styles.boxPrimary} />
+      <div className={styles.boxSecondary} />
+      <div className={styles.boxDark} />
+      <div className={styles.boxAccent} />
+    </div>
+  );
+}
