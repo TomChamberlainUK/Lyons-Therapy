8222f0a0f33c77a664d2576b2a2cd54ac26f1d73
Tom Chamberlain <tom_chamberlain_uk@hotmail.co.uk>
2021-07-14 13:13:00 +0100

    Removed preventing default event behaviour when button is pressed. Added 'type' prop

--- src/components/button/Button.js
+++ src/components/button/Button.js
@@ -6,17 +6,17 @@ export function ButtonSet({ children }) {
   return <div className={styles.buttonSet}>{children}</div>
 }
 
-export function Button({ children, isPrimary, icon, onClick }) {
+export function Button({ children, type = "button", onClick, icon, isPrimary }) {
 
   function handleClick(event) {
-    event.preventDefault();
-    if (onClick) onClick();
+    if (onClick) onClick(event);
   }
 
   return (
     <button
       className={`${styles.button} ${!isPrimary && styles.buttonSecondary}`}
       onClick={handleClick}
+      type={type}
     >
       {icon && <i className={`material-icons ${styles.button__icon}`}>{icon}</i>}
       {children}
