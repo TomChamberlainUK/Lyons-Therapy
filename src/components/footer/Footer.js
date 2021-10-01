282bb8605ff6191e13b863c786958022f20c8474
Tom Chamberlain <tom_chamberlain_uk@hotmail.co.uk>
2021-10-01 14:39:43 +0100

    Added placeholder links in legal strip of footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'gatsby';
 import { StaticImage } from 'gatsby-plugin-image';
 
 import * as styles from './footer.module.scss';
@@ -14,7 +15,10 @@ function Footer() {
         className={styles.bacpBadge}
       />
       <div className={styles.legalStrip}>
-        &copy; Lyons Therapy
+        <span>&copy; Lyons Therapy</span>
+        <span>
+          <Link to="#" style={{color: 'rgba(255,255,255,1'}}>Privacy Policy</Link> <Link to="#" style={{color: 'rgba(255,255,255,1'}}>Terms of Use</Link> <Link to="#" style={{color: 'rgba(255,255,255,1'}}>Cookies</Link>
+        </span>
       </div>
     </footer>
   )
