d6a5b9eda48db9e004e4d755daa46aa95d17a98a
Tom Chamberlain <tom_chamberlain_uk@hotmail.co.uk>
2021-10-01 16:43:03 +0100

    Fixed aside cards so that more than 3 can appear in desktop view. Also removed the need to calculate scroll-bar width for simplicity

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -8,13 +8,6 @@ import Card from 'components/card/Card';
 import * as styles from './layout.module.scss';
 import 'styles/global.scss';
 
-function calculateScrollBarWidth() {
-  document.documentElement.style.setProperty('--size-scrollbar-width', (window.innerWidth - document.documentElement.clientWidth) + 'px');
-}
-window.addEventListener('resize', calculateScrollBarWidth, false);
-document.addEventListener('DOMContentLoaded', calculateScrollBarWidth, false);
-document.addEventListener('load', calculateScrollBarWidth);
-
 function Layout({ children, pageTitle }) {
   return (
     <div className={styles.root}>
@@ -24,6 +17,9 @@ function Layout({ children, pageTitle }) {
         {children}
       </main>
       <aside className={styles.asideCards}>
+        <Card>With obstacles removed, each of us have a propensity to move towards healing, change and growth.</Card>
+        <Card>With obstacles removed, each of us have a propensity to move towards healing, change and growth.</Card>
+        <Card>With obstacles removed, each of us have a propensity to move towards healing, change and growth.</Card>
         <Card>With obstacles removed, each of us have a propensity to move towards healing, change and growth.</Card>
         <Card>With obstacles removed, each of us have a propensity to move towards healing, change and growth.</Card>
         <Card>With obstacles removed, each of us have a propensity to move towards healing, change and growth.</Card>
