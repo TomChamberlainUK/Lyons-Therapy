bba8e1632ee673c176107defaef5b90d9a430a9a
Tom Chamberlain <tom_chamberlain_uk@hotmail.co.uk>
2021-11-26 19:35:24 +0000

    Fixed issues with gatsby StaticImages on index

--- src/components/indexContainer/IndexContainer.js
+++ src/components/indexContainer/IndexContainer.js
@@ -8,29 +8,35 @@ export default function IndexContainer({ children }) {
   return (
     <div className={styles.indexContainer}>
       <div className={styles.imageFrame}>
-        <StaticImage
-          src="../../images/toby-face.jpg"
-          alt="PLACEHOLDER IMAGE"
-          placeholder="blurred"
-          className={styles.image}
-          loading="eager"
-        />
-        <StaticImage
-          src="../../images/logo-circular-accent.png"
-          alt="PLACEHOLDER IMAGE"
-          placeholder="blurred"
-          className={styles.logoDesktop}
-          loading="eager"
-        />
+        <div className={styles.imageWrapper}>
+          <StaticImage
+            src="../../images/toby-face.jpg"
+            alt="PLACEHOLDER IMAGE"
+            placeholder="blurred"
+            className={styles.image}
+            loading="eager"
+          />
+        </div>
+        <div className={styles.logoDesktopWrapper}>
+          <StaticImage
+            src="../../images/logo-circular-accent.png"
+            alt="PLACEHOLDER IMAGE"
+            placeholder="blurred"
+            className={styles.logoDesktop}
+            loading="eager"
+          />
+        </div>
       </div>
       <div className={styles.textArea}>
-        <StaticImage
-          src="../../images/logo-circular-accent.png"
-          alt="PLACEHOLDER IMAGE"
-          placeholder="blurred"
-          className={styles.logoMobile}
-          loading="eager"
-        />
+        <div className={styles.logoMobileWrapper}>
+          <StaticImage
+            src="../../images/logo-circular-accent.png"
+            alt="PLACEHOLDER IMAGE"
+            placeholder="blurred"
+            className={styles.logoMobile}
+            loading="eager"
+          />
+        </div>
         {children}
       </div>
     </div>
