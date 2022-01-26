66f99fb17790d5c25327ddc6936e800ee75621bc
Tom Chamberlain <tom_chamberlain_uk@hotmail.co.uk>
2022-01-26 17:22:40 +0000

    Added prop for pinning subcontent

--- src/components/splitContainer/SplitContainer.js
+++ src/components/splitContainer/SplitContainer.js
@@ -2,7 +2,7 @@ import React, { forwardRef } from 'react';
 
 import * as styles from './splitContainer.module.scss';
 
-export const SplitContainer = forwardRef(function SplitContainer({ children, subContent, columnReverse }, ref) {
+export const SplitContainer = forwardRef(function SplitContainer({ children, subContent, columnReverse, pinSubContent }, ref) {
   return (
     <div
       className={`
@@ -14,7 +14,7 @@ export const SplitContainer = forwardRef(function SplitContainer({ children, sub
       <div className={styles.splitContainer__primary}>
         {children}
       </div>
-      <div className={styles.splitContainer__secondary}>
+      <div className={`${styles.splitContainer__secondary} ${pinSubContent && styles.splitContainer__secondaryPinned}`}>
         {subContent}
       </div>
     </div>
