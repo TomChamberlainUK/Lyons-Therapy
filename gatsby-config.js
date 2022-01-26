e4f1533523a592b1f2e83949d3b01e0ad0e467fa
Tom Chamberlain <tom_chamberlain_uk@hotmail.co.uk>
2022-01-26 20:58:47 +0000

    Added manifest and favicons

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,6 +3,17 @@ module.exports = {
     title: "Lyons Therapy",
   },
   plugins: [
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        name: "Lyons Therapy",
+        short_name: "Lyons Therapy",
+        start_url: "/",
+        background_color: "#fff",
+        theme_color: "#fff",
+        icon: "src/images/favicon.svg",
+      }
+    },
     "gatsby-plugin-react-helmet",
     "gatsby-plugin-sass",
     "gatsby-plugin-gatsby-cloud",
